ac7efa902ea09259c5bc4125f32ddcd45ade1d5c
bhanuteja449896 <bhanutejamakkineni@gmail.com>
2024-10-11 10:57:44 +0530

    Checking if already registered or not

--- src/main/java/com/example/db/service/CustomerService.java
+++ src/main/java/com/example/db/service/CustomerService.java
@@ -14,12 +14,22 @@ public class CustomerService {
     CustomerRepository customerRepository;
 
     public CustRegResp custRegistration(CustRegReq req) {
-        Customer c = new Customer();
-        c.setDob(req.getDob());
-        c.setGmail(req.getGmail());
-        c.setName(req.getName());
-        c.setMobile(req.getMobile());
-        customerRepository.save(c);
+        Customer c=customerRepository.findByMobile(req.getMobile());
+        if ( c!= null ){
+            CustRegResp custRegResp = new CustRegResp();
+            custRegResp.setRc("01");
+            custRegResp.setDesc("Customer Already Registered");
+            return custRegResp;
+        }
+        else {
+            c = new Customer();
+            c.setDob(req.getDob());
+            c.setGmail(req.getGmail());
+            c.setName(req.getName());
+            c.setMobile(req.getMobile());
+            customerRepository.save(c);
+        }
+
         CustRegResp custRegResp = new CustRegResp();
         custRegResp.setRc("00");
         custRegResp.setDesc("Success");
